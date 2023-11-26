179397a7378465e6de362f06c2a57d1bf137c089
Mayank Tripathi <tripathimayank216@gmail.com>
2023-11-26 12:38:00 +0530

    Update task_data

--- lib/components/task.dart
+++ lib/components/task.dart
@@ -7,4 +7,12 @@ class Task {
   void toggleDone(){
     isDone = !isDone;
   }
+
+  Map<String, dynamic> toMap() {
+    return {'name': name, 'isDone': isDone};
+  }
+
+  factory Task.fromMap(Map<String, dynamic> map) {
+    return Task(name: map['name'], isDone: map['isDone']);
+  }
 }
--- lib/components/task_data.dart
+++ lib/components/task_data.dart
@@ -1,34 +1,70 @@
+import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'task.dart';
 import 'dart:collection';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class TaskData extends ChangeNotifier {
-  final List<Task> _tasks = [
-    Task(name: 'Welcome to NovaTask!'),
-    Task(name: 'Click \'+\' to add a new item'),
-    Task(name: 'Long press on any item to delete'),
-  ];
+  final List<Task> _tasks = [];
+
+  TaskData() {
+    _loadTasks();
+  }
 
   UnmodifiableListView<Task> get tasks {
     return UnmodifiableListView(_tasks);
-}
+  }
 
   int get taskCount {
     return _tasks.length;
   }
 
-  void addTask(String newTitle){
+  void addTask(String newTitle) {
     _tasks.add(Task(name: newTitle));
+    _saveTasks();
     notifyListeners();
   }
 
-  void updateTask(Task task){
+  void updateTask(Task task) {
     task.toggleDone();
+    _saveTasks();
     notifyListeners();
   }
 
-  void deleteTask(Task task){
+  void deleteTask(Task task) {
     _tasks.remove(task);
+    _saveTasks();
     notifyListeners();
+  }
+
+  void _saveTasks() async {
+    final prefs = await SharedPreferences.getInstance();
+    final taskList = _tasks.map((task) => task.toMap()).toList();
+    prefs.setString('tasks', jsonEncode(taskList));
+  }
+
+
+  void _loadTasks() async {
+    final prefs = await SharedPreferences.getInstance();
+    final bool isFirstLaunch = prefs.getBool('firstLaunch') ?? true;
+
+    if (isFirstLaunch) {
+      _addDefaultTasks();
+      prefs.setBool('firstLaunch', false);
+    } else {
+      final String? taskListString = prefs.getString('tasks');
+
+      if (taskListString != null) {
+        final Iterable decoded = jsonDecode(taskListString);
+        _tasks.addAll(decoded.map((task) => Task.fromMap(task)));
+        notifyListeners();
+      }
+    }
+  }
+
+  void _addDefaultTasks() {
+    addTask('Welcome to NovaTask!');
+    addTask('Click \'+\' to add a new item');
+    addTask('Long press on any item to delete');
   }
 }
